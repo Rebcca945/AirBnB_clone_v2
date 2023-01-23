6b6c2743df41913ed513e38fd51f142e2b1c826d
Rebcca945 <106800841+Rebcca945@users.noreply.github.com>
2023-01-23 18:45:07 +0300

    Update 2-do_deploy_web_static.py
    AirBnB clone

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["54.225.241.125", "54.236.28.55"]
+env.hosts = ["54.157.32.137", "52.55.249.213"]
 
 
 def do_deploy(archive_path):
